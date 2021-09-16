c724a2b058f5ae15f666a32dcc306b0ce90e650e
Nobel Gebru <34787650+Nobel-Gebru@users.noreply.github.com>
2021-09-16 17:41:01 -0400

    first draft of webpage in python
    just initial questions to start the program off

---
+++ First Draft.py
@@ -0,0 +1,9 @@
+def greeting():
+    name=str(input("What is your name? : "))
+    #print("Hello " + str(name))
+    day=str(input("Hi " + name + " How was your day? : "))
+    quality=str(input("why was your day " + day + "?"))
+    activity=str(input(" & what did you do today?"))
+    return
+
+greeting()
